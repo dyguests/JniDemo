10e9ad0d029073549ced280052154b96289042e4
fanhl <dyguests@qq.com>
2018-09-23 16:22:45 +0800

    std::cout

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
 
 extern "C" JNIEXPORT jstring
 
@@ -8,5 +9,8 @@ Java_com_fanhl_jnidemo_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    std::cout << "Java_com_fanhl_jnidemo_MainActivity_stringFromJNI_cout\n";
+
     return env->NewStringUTF(hello.c_str());
 }
